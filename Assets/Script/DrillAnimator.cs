5d43556f366dbbbc168a0826baac51d340acfb56
Dmitrii <drozdovdm27@gmail.com>
2025-04-06 13:25:42 +0300

    Drilling animation

---
+++ Assets/Script/DrillAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class DrillAnimator : MonoBehaviour
+{
+    public SpriteRenderer SpriteRenderer;
+    public Sprite[] Sprites;
+    public float ChangeSpriteSpeedMultiplayer;
+
+    private int _currentSpriteIndex;
+    private float _currentTimer;
+    
+    private void Update()
+    {
+        if (_currentTimer >= CalculateNextTimeToSprite())
+        {
+            _currentSpriteIndex++;
+            if (_currentSpriteIndex >= Sprites.Length)
+                _currentSpriteIndex = 0;
+
+            SpriteRenderer.sprite = Sprites[_currentSpriteIndex];
+            _currentTimer = 0f;
+        }
+
+        _currentTimer += Time.deltaTime;
+    }
+
+    private float CalculateNextTimeToSprite()
+    {
+        float speedPercent = Game.Instance.DrillAccelerator.CurrentSpeed / Game.Instance.GameStats.MaxSpeed;
+        return (1 - speedPercent) * ChangeSpriteSpeedMultiplayer;
+    }
+}
